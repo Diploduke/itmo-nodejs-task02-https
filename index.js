731517df2aa2c70f0194fa18fd42ffb41571553b
Diploduke <60945085+Diploduke@users.noreply.github.com>
2021-05-12 01:48:54 +0300

    Первый коммит

---
+++ index.js
@@ -0,0 +1,32 @@
+const [{ Server: h1 }, x] = [require('http'), require('express')];
+// const x = require('express');
+const app = x();
+// const router = x.Router();
+const PORT = 4321;
+// const { log } = console;
+const huc = { 
+  'Content-Type': 'text/html; charset=utf-8',
+  'Access-Control-Allow-Origin': '*'
+};
+const tuc = { 
+  'Content-Type': 'text/plain; charset=utf-8',
+  'Access-Control-Allow-Origin': '*' 
+};
+
+app
+  // .all((r, rs) => rs.status(200).set(tuc))
+  .get('/', (r, rs) => { rs.status(200).set(tuc).end('79213589765') })
+  .get('/login/', (r, rs) => { rs.status(200).set(tuc).end('79213589765') })
+  .get('/sample/', (r, rs) => { 
+    const textFunction = `function task(x) {
+        return x * Math.pow(this, 2);
+      }`;
+    rs.status(200).set(tuc).end(textFunction)
+    // rs.end = textFunction + '\n';
+  })
+
+  .use((r, rs, n) => { r.errorMessage = 'Не найдено!'; next(); })
+  .use(r => r.res.status(404).set(huc).send(r.errorMessage))
+  .use((e, r, rs, n) => rs.status(500).set(huс).send(`Ошибка: ${e}`))
+
+  .listen(4321);
