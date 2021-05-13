dc18747a5aaf26fb5402625e84bc03fdc39b1efc
Diploduke <60945085+Diploduke@users.noreply.github.com>
2021-05-13 21:26:21 +0300

    7-й коммит

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const [{ Server: h1 }, x] = [require('http'), require('express')];
 const app = x();
 // const router = x.Router();
 const PORT = 4321;
-// const { log } = console;
+const { log } = console;
 const huc = { 
   'Content-Type': 'text/html; charset=utf-8',
   'Access-Control-Allow-Origin': '*'
@@ -14,19 +14,19 @@ const tuc = {
 };
 
 app
-  .get('/', (r, rs) => { rs.status(200).set(tuc).end('79213589765') })
-  .get('/login/', (r, rs) => { rs.status(200).set(tuc).end('79213589765') })
-  .get('/sample/', (r, rs) => { 
+  .get('/', (r, rs, n) => { rs.status(200).set(tuc).end('79213589765') })
+  .get('/login/', (r, rs, n) => { rs.status(200).set(tuc).end('79213589765') })
+  .get('/sample/', (r, rs, n) => { 
     const textFunction = `function task(x) {
         return x * Math.pow(this, 2);
       }`;
     rs.status(200).set(tuc).end(textFunction)
   })
 
-  .use((r, rs, n) => { r.errorMessage = 'Не найдено!'; next(); })
+  .use((r, rs, n) => { r.errorMessage = 'Не найдено!'; n(); })
   .use(r => r.res.status(404).set(huc).send(r.errorMessage))
-  .use((e, r, rs, n) => rs.status(500).set(huс).send(`Ошибка: ${e}`))
-
+  .use((e, r, rs, n) => rs.status(500).set(huc).send(`Ошибка: ${e}`))
+  // .set(tuc);
   .listen(4321);
 // module.exports = h1(app)
 //   .listen(process.env.PORT || PORT, () => log(process.pid));
